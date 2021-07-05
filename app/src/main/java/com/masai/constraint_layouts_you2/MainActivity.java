c65a3bd01bede42890ee0e770c579631ed25fcf0
Avadhutjc <Avadhutjc@gmail.com>
2021-07-06 00:25:28 +0530

    first commit

---
+++ app/src/main/java/com/masai/constraint_layouts_you2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.masai.constraint_layouts_you2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
